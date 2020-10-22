7ba626b92ca0b562cd19b5f4f3633d2106887b2e
Marcos-Aquino <marcos-aquino@live.com>
2020-10-22 19:53:44 -0300

    Atual

--- gerenciaArquivo.py
+++ gerenciaArquivo.py
@@ -1,28 +1,33 @@
 #########################################################
 #   Trabalho DCC146 - Aspectos Teoricos da Computacao   #
 # Grupo:                                                #
-# Ian Couto - 201876002                                 #
-# Marcos Aquino - 201276024                             #
+# Marcos Aquino                                         #
 #########################################################
 
+import validacaoTags as valida
 import os
 
 #Le um arquivo de tags, valida e carrega as validas na memoria
 def importaArquivo(nomeArquivo, tags):
-    print("[ERRO] salvaArquivo ainda não foi implementado")
-    #implemenar função que le um arquivo de tags, valida e carrega as validas na memoria
+    try:
+        with open(nomeArquivo) as arquivo:
+            for linha in arquivo:
+                if valida.verificaFormato(linha, tags):
+                    if valida.validaTag(linha):
+                        tags.append(linha)
+        print ('[INFO] As definicoes de tags foram carregadas')
+    except(IOError):
+       print ("[WARN] Erro ao abrir o arquivo:", nomeArquivo)
 
 #Salva em um arquivo todas as tags que foram validadas
 def salvaArquivo (nomeArquivo, tags):
-    print("[ERRO] salvaArquivo ainda não foi implementado")
-    #implemenar função que salva as tags em arquivo de saida
-
-#Importa o texto para ser dividido
-def importaDivisao(nomeArquivo, texto):
-    print("[ERRO] importaDivisão ainda não foi implementado")
-    #implemenar função que divide o texto em tags
-
-#Salva em um arquivo todas as tags que foram divididas
-def salvaArquivoDivisao (nomeArquivo, tags):
-    print("[ERRO] salvaArquivoDivisão ainda não foi implementado")
-    #implemenar função que salva em um arquivo todas as tags que foram divididas
+    if os.path.isfile(nomeArquivo):
+        print('[WARN] O arquivo', nomeArquivo, 'ja existe e sera sobrescrito!')
+    try:
+        arquivo = open(nomeArquivo, "w") #w apaga e escreve por cima do arquivo caso ele ja exista
+        for tag in tags:
+            arquivo.write(tag)
+        arquivo.close()
+        print ('[INFO] As definicoes de tags foram salvas')
+    except(IOError):
+        print ("[WARN] Erro ao salvar o arquivo:", nomeArquivo)
--- main.py
+++ main.py
@@ -5,7 +5,8 @@
 # Marcos Aquino - 201276024                             #
 #########################################################
 
-import os
+import validacaoTags as valida
+import gerenciaArquivo as arquivo
 
 def printMenu():
     print("\nComando|                           Descrição                           |Exemplo")
@@ -18,6 +19,9 @@ def printMenu():
     print(":q     |sair do programa                                               |:q")
     print(":s     |salvar as tags                                                 |:s file.txt\n")
 
+if __name__ == "__main__":
+    conjunto_tags = [] #Conjunto de tags validas (tags invalidas nao sao armazenadas)
+
 #Le entradas do usuario ate que o comando :q seja digitado
 while True:
     printMenu()
@@ -26,39 +30,34 @@ while True:
         entrada = input()
     #Reconhece os comandos iniciados com ':'
     if entrada[0] == ':':
-        comando = entrada.split()
-        if comando[0] == ':d':
-            if len(comando) != 2:
-                print ('[WARWING] Este comando precisa de 1 parametro!')
+            comando = entrada.split()
+            if comando[0] == ':q':
+                print ('[INFO] Encerrando programa.')
+                quit()
+            elif comando[0] == ':s':
+                if len(comando) != 2:
+                    print ('[WARN] Este comando precisa de 1 parametro!')
+                else:
+                    arquivo.salvaArquivo(comando[1], conjunto_tags)
+            elif comando[0] == ':l':
+                if len(comando) != 2:
+                    print ('[WARN] Este comando precisa de 1 parametro!')
+                else:
+                    arquivo.importaArquivo(comando[1], conjunto_tags)
+            elif comando[0] == ':d':
+                print ('[INFO] Comando para realizar a divisao de tags do arquivo ainda nao implementado!')
+            elif comando[0] == ':o':
+                print ('[INFO] Comando para especificar o caminho do arquivo de saida ainda nao implementado!')
+            elif comando[0] == ':p':
+                print ('[INFO] Comando para realizar a divisao de tags da entrada ainda nao implementado!')
+            elif comando[0] == ':a':
+                print ('[INFO] Comando para listar as definições formais dos autômatos em memória ainda nao implementado!')
+            elif comando[0] == ':c':
+                print ('[INFO] Comando para carregar um arquivo com definições de tags ainda nao implementado!')                
             else:
-                print("[INFO] executar comando :d não implementado")
-        elif comando[0] == ':c':
-            if len(comando) != 2:
-                print ('[WARWING] Este comando precisa de 1 parametro!')
-            else:
-                print("[INFO] executar comando :c não implementado")
-        elif comando[0] == ':o':
-            if len(comando) != 2:
-                print ('[WARWING] Este comando precisa de 1 parametro!')
-            else:
-                print("[INFO] executar comando :o não implementado")
-        elif comando[0] == ':p':
-            if len(comando) != 2:
-                print ('[WARWING] Este comando precisa de 1 parametro!')
-            else:
-                print("[INFO] executar comando :p não implementado")
-        elif comando[0] == ':a':
-            print("[INFO] executar comando :a não implementado")
-        elif comando[0] == ':l':
-            print("[INFO] executar comando :l não implementado")
-        elif comando[0] == ':q':
-            print("[INFO] executar comando :q não implementado")
-            quit()
-        elif comando[0] == ':s':
-            if len(comando) != 2:
-                print ('[WARWING] Este comando precisa de 1 parametro!')
-            else:
-                print("executar comando :s não implementado")
-    #Usuario digitou uma tag (do tipo VAR: ab+c+x+) diretamente e ela precisa ser validada
+                print ('[ERROR] Comando invalido!')
+        #Usuario digitou uma tag (do tipo VAR: ab+c+x+) diretamente e ela precisa ser validada
     else:
-        print("tag não reconhecida")
+        if valida.verificaFormato(entrada, conjunto_tags):
+            if valida.validaTag(entrada):
+                conjunto_tags.append(entrada+'\n')
